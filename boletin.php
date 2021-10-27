f6eebca0df6bebd18212a52213dbe185b9fbf385
Edgar Hernández Martínez <edgaarhdez96@gmail.com>
2021-10-27 14:20:25 -0500

    Plantilla Boletín

--- boletin.php
+++ boletin.php
@@ -26,40 +26,40 @@
 <!-- /jumbotron -->
 <!-- ==== Page Content ==== -->
 <div class="page container">
-<p style="text-align: justify" class="element-heading col-lg-12">Les presentamos el Boletín Institucional de La Huella que hemos armado junto a todos los integrantes de los proyectos que aquí funcionan.
-La idea del Boletín surge con la intención de fomentar la comunicación, hacer propios los distintos proyectos de La Huella, compartir sueños, motivaciones y expectativas y también surge con la finalidad de compartir las novedades con todos aquellos que no forman parte del día a día.</p>
+<p style="text-align: justify" class="col-lg-12">Les presentamos el Boletín Institucional de La Huella que hemos armado junto a todos los integrantes de los proyectos que aquí funcionan.
+La idea del Boletín surge con la intención de fomentar la comunicación, hacer propios los distintos proyectos de La Huella, compartir sueños, motivaciones y expectativas y también surge con la finalidad de compartir las novedades con todos aquellos que no forman parte del día a día.</p><br>
     <div class="row">
         <br>
         <h5>Boletines disponibles</h5>
     </div>
     <div class="row">
-    <div class="row" style="display: flex; justify-content: center;">
-        <li><a class="custom-link float-right mt-5" href="https://drive.google.com/file/d/10RrqyKrBiXkgvg-c1aUY6fDBhEgi_j6b/view?usp=sharing"target="_blank">Boletín 1-Abril 2014</a></li>
-
-        <li><a class="custom-link float-right mt-5" href="http://lahuella.org.uy/boletines/2%C2%BA%20boletin%20setiembre%202014.pdf" target="_blank"target="_blank">Boletín 2-Septiembre 2014</a></li>
-
-        <li><a class="custom-link float-right mt-5" href="https://drive.google.com/file/d/1kTHH7TqbgdV4EXgsIYM573CIGN6yUntW/view?usp=sharing" target="_blank"target="_blank">Boletín 3-Marzo 2015</a></li>
-
-        <li><a class="custom-link float-right mt-5" href="https://drive.google.com/file/d/1bBoZZVhZbqDBkAapYhxWMzXT3yQMlCm2/view?usp=sharing" target="_blank"target="_blank">Boletín 4-Septiembre 2017</a></li>
-
-        <li><a class="custom-link float-right mt-5" href="https://drive.google.com/file/d/10OYryyIZ0oYVmrstbcUbSJCWnEt-LDhM/view?usp=sharing" target="_blank"target="_blank">Boletín 5-Diciembre 2017</a></li>
-
-        <li><a class="custom-link float-right mt-5" href="https://drive.google.com/file/d/1MNmAtQMmljO3tGCwXqov9du_IvozuRfc/view?usp=sharing" target="_blank"target="_blank">Boletín 6-Mayo 2018</a></li>
-
-        <li><a class="custom-link float-right mt-5" href="https://drive.google.com/file/d/1CWVwtBwJH9bUp1dqh4GPLq2OIqK1DMu7/view?usp=sharing" target="_blank"target="_blank">Boletín 7-Septiembre 2018</a></li>
-
-        <li><a class="custom-link float-right mt-5" href="https://drive.google.com/file/d/1kOt9hLq7dnZev8leDFPh8cn9Ouc6e3kV/view?usp=sharing" target="_blank"target="_blank">Boletín 8-Diciembre 2018</a></li>
-
-        <li><a class="custom-link float-right mt-5" href="https://drive.google.com/file/d/1xndRNZcDrJ8_T_I-57UtPYRGv_a-F77q/view?usp=sharing" target="_blank"target="_blank">Boletín 9-Abril 2019</a></li>
-
-        <li><a class="custom-link float-right mt-5" href="https://drive.google.com/file/d/1vbazPUCT59XGo5ECOJue1rWykEVfuNII/view?usp=sharing" target="_blank"target="_blank">Boletín 10-Agosto 2019</a></li>
-
-        <li><a class="custom-link float-right mt-5" href="https://drive.google.com/file/d/1sVOdYdhqlsUFQ3qptClNHGHd-j4GFJH1/view?usp=sharing" target="_blank"target="_blank">Boletín 11-Diciembre 2019</a></li>
-        <li><a class="custom-link float-right mt-5" href="https://drive.google.com/file/d/11s7pulMzPs80Fkjj02gli33zqwG_W6PX/view?usp=sharing" target="_blank"target="_blank">Boletín 12-Mayo 2020</a></li>
-        
-        <li><a class="custom-link float-right mt-5" href="https://drive.google.com/file/d/1De8p-BdGagbpELnQtu_UIP4nV0K29pub/view?usp=sharing" target="_blank">Boletín 13-Octubre 2020</a></li>
-
-       </div>
+      <div class="comparisontable">
+         <ul class="row">
+            <li><img src="https://image.ibb.co/ksjS3y/Zenith_chair.jpg" /><br /><a class="custom-link mt-5" href="https://drive.google.com/file/d/10RrqyKrBiXkgvg-c1aUY6fDBhEgi_j6b/view?usp=sharing"target="_blank">Boletín 1-Abril 2014</a></li>
+            <li><img src="https://image.ibb.co/ksjS3y/Zenith_chair.jpg" /><br /><a class="custom-link mt-5" href="http://lahuella.org.uy/boletines/2%C2%BA%20boletin%20setiembre%202014.pdf" target="_blank">Boletín 2-Septiembre 2014</a></li>
+            <li><img src="https://image.ibb.co/ksjS3y/Zenith_chair.jpg" /><br /><a class="custom-link mt-5" href="https://drive.google.com/file/d/1kTHH7TqbgdV4EXgsIYM573CIGN6yUntW/view?usp=sharing" target="_blank">Boletín 3-Marzo 2015</a></li>
+         </ul>
+         <ul class="row">
+            <li><img src="https://image.ibb.co/ksjS3y/Zenith_chair.jpg" /><br /><a class="custom-link mt-5" href="https://drive.google.com/file/d/1bBoZZVhZbqDBkAapYhxWMzXT3yQMlCm2/view?usp=sharing" target="_blank">Boletín 4-Septiembre 2017</a></li>
+            <li><img src="https://image.ibb.co/ksjS3y/Zenith_chair.jpg" /><br /><a class="custom-link mt-5" href="https://drive.google.com/file/d/10OYryyIZ0oYVmrstbcUbSJCWnEt-LDhM/view?usp=sharing" target="_blank">Boletín 5-Diciembre 2017</a></li>
+            <li><img src="https://image.ibb.co/ksjS3y/Zenith_chair.jpg" /><br /><a class="custom-link mt-5" href="https://drive.google.com/file/d/1MNmAtQMmljO3tGCwXqov9du_IvozuRfc/view?usp=sharing" target="_blank">Boletín 6-Mayo 2018</a></li>
+         </ul>
+         <ul class="row">
+            <li><img src="https://image.ibb.co/ksjS3y/Zenith_chair.jpg" /><br /><a class="custom-link mt-5" href="https://drive.google.com/file/d/1CWVwtBwJH9bUp1dqh4GPLq2OIqK1DMu7/view?usp=sharing" target="_blank">Boletín 7-Septiembre 2018</a></li>
+            <li><img src="https://image.ibb.co/ksjS3y/Zenith_chair.jpg" /><br /><a class="custom-link mt-5" href="https://drive.google.com/file/d/1kOt9hLq7dnZev8leDFPh8cn9Ouc6e3kV/view?usp=sharing" target="_blank">Boletín 8-Diciembre 2018</a></li>
+            <li><img src="https://image.ibb.co/ksjS3y/Zenith_chair.jpg" /><br /><a class="custom-link mt-5" href="https://drive.google.com/file/d/1xndRNZcDrJ8_T_I-57UtPYRGv_a-F77q/view?usp=sharing" target="_blank">Boletín 9-Abril 2019</a></li>
+         </ul>
+         <ul class="row">
+            <li><img src="https://image.ibb.co/ksjS3y/Zenith_chair.jpg" /><br /><a class="custom-link mt-5" href="https://drive.google.com/file/d/1vbazPUCT59XGo5ECOJue1rWykEVfuNII/view?usp=sharing" target="_blank">Boletín 10-Agosto 2019</a></li>
+            <li><img src="https://image.ibb.co/ksjS3y/Zenith_chair.jpg" /><br /><a class="custom-link mt-5" href="https://drive.google.com/file/d/1sVOdYdhqlsUFQ3qptClNHGHd-j4GFJH1/view?usp=sharing" target="_blank">Boletín 11-Diciembre 2019</a></li>
+            <li><img src="https://image.ibb.co/ksjS3y/Zenith_chair.jpg" /><br /><a class="custom-link mt-5" href="https://drive.google.com/file/d/11s7pulMzPs80Fkjj02gli33zqwG_W6PX/view?usp=sharing" target="_blank">Boletín 12-Mayo 2020</a></li>
+         </ul>
+         <ul class="row">
+            <li></li>
+            <li><img src="https://image.ibb.co/ksjS3y/Zenith_chair.jpg" /><br /><a class="custom-link mt-5" href="https://drive.google.com/file/d/1De8p-BdGagbpELnQtu_UIP4nV0K29pub/view?usp=sharing" target="_blank">Boletín 13-Octubre 2020</a></li>
+            <li></li>
+         </ul>
+      </div>
     </div>
 </div>
 
