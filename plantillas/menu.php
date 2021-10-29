0d4178024ab2ec5a4cc2c519707a0c521378ea01
Edgar Hernández Martínez <edgaarhdez96@gmail.com>
2021-10-29 15:17:26 -0500

    Florencia Ramos

--- plantillas/menu.php
+++ plantillas/menu.php
@@ -4,13 +4,12 @@
     <div id="preloader">
         <div class="container h-100">
             <div class="row h-100 justify-content-center align-items-center">
-                <div class="preloader-logo">
-                    <!-- spinner -->
+                <!-- <div class="preloader-logo">
                     <div class="spinner">
                         <div class="dot1"></div>
                         <div class="dot2"></div>
                     </div>
-                </div>
+                </div> -->
                 <!--/preloader logo -->
             </div>
             <!--/row -->
